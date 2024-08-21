f1421a646e4c2ecc0de378d355df5c66ce3ac2d9
Suki <tues1205@naver.com>
2024-08-21 21:08:19 +0900

    LoginPage useNavigate()

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,42 +2,51 @@ import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faUser } from '@fortawesome/free-regular-svg-icons'
 import { faSearch } from '@fortawesome/free-solid-svg-icons'
-
-
+import { useNavigate } from 'react-router-dom'
 
 const Navbar = () => {
 
     const menuList= ['NEW', 'ANIMALS', 'AMUSEABLES', 'BAGS & CHARMS', 'COLLECTIONS'];
 
-  return (
-    <div>
-        <div className='top-bar'>
-            <div className='login-button'>
-                <FontAwesomeIcon icon={faUser} />
-            </div>  
-        </div>
+    const navigate = useNavigate();
 
-        <div className='logo-img'>
-            <img width={200} src='https://cdn11.bigcommerce.com/s-fz2bnmwg7y/images/stencil/250x100/jellycat-logo-250x100_1707353282__97555.original.png' />
-        </div>
+    const goToLoginpage =()=> {
+        navigate("/login");
+    }
 
-        <div className='menu-area'>
+    const goToHomepage =()=> {
+        navigate("/");
+    }
 
-            <div className='search-box'>
-                <FontAwesomeIcon icon={faSearch} className='search-btn'/>
-                <input type='text' className='search-input' placeholder='Search..'/>
+    return (
+        <div>
+            <div className='top-bar'>
+                <div className='login-button'>
+                    <FontAwesomeIcon icon={faUser} onClick={goToLoginpage}/>
+                </div>  
             </div>
 
-            <ul className='menu-list'>
-                {menuList.map((menu) => (
-                    <li>{menu}</li>
-                ))}
-            </ul>
+            <div className='logo-img'>
+                <img width={200} src='https://cdn11.bigcommerce.com/s-fz2bnmwg7y/images/stencil/250x100/jellycat-logo-250x100_1707353282__97555.original.png' onClick={goToHomepage}/>
+            </div>
 
-            
+            <div className='menu-area'>
+
+                <div className='search-box'>
+                    <FontAwesomeIcon icon={faSearch} className='search-btn'/>
+                    <input type='text' className='search-input' placeholder='Search..'/>
+                </div>
+
+                <ul className='menu-list'>
+                    {menuList.map((menu) => (
+                        <li>{menu}</li>
+                    ))}
+                </ul>
+
+                
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Navbar
