436835fbe2d1f1b0f04218ef7f158cbb54bcf57e
Suki <tues1205@naver.com>
2024-08-21 18:12:03 +0900

    db.json

---
+++ src/components/ProductCard.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ProductCard = () => {
+  return (
+    <div>
+        <img src='https://cdn11.bigcommerce.com/s-fz2bnmwg7y/images/stencil/320w/products/3939/109803/BAS3SOR__64676.1.jpg' />
+        <div>제목</div>
+        <div>가격</div>
+        <div>신제품</div>
+
+    </div>
+  )
+}
+
+export default ProductCard
--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,9 +1,27 @@
 import React from 'react'
+import { useState, useEffect } from 'react';
+import ProductCard from '../components/ProductCard';
 
 const MainPage = () => {
+
+  const [productList, setProductList] = useState([]);
+
+  const getProducts = async() => {
+    const response = await fetch('http://localhost:5000/products');
+    const data = await response.json();
+    console.log(data);
+
+    setProductList(data);
+  };
+
+  useEffect(()=> {
+    getProducts()
+
+  }, [])
+
   return (
     <div>
-        전체 상품 페이지
+        <ProductCard />
     </div>
   )
 }
